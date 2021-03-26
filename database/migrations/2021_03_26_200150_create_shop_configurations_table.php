bd4884eb6ca92b8541ab4d13c1264e40dab2af96
naumanxds <nauman.nasir919@gmail.com>
2021-03-27 01:10:03 +0500

    make table for shop configurations

---
+++ database/migrations/2021_03_26_200150_create_shop_configurations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopConfigurationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_configurations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('shop_url')->unique();
+            $table->text('client_id')->nullable();
+            $table->text('client_secret')->nullable();
+            $table->text('public_token')->nullable();
+            $table->text('secret_token')->nullable();
+            $table->integer('number_field')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id', 'shop_configurations_n_users_fk')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_configurations');
+    }
+}
